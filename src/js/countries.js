fbf529db5dcd92fe5badb2a9b1975abdca7add94
Filip <filip.bergander1996@gmail.com>
2026-02-24 17:30:55 +0100

    Skapat felmeddelanden om landet inte hittas eller om användaren inte söker på något.

--- src/js/countries.js
+++ src/js/countries.js
@@ -1,28 +1,43 @@
 "use strict";
 
-
-
 addEventListener("DOMContentLoaded", () => {
     const countryInput = document.getElementById("country-name");
     const searchBtn = document.getElementById("search-button");
     const searchError = document.getElementById("country-error");
     searchBtn.addEventListener("click", () => {
-        countryInput.value = countryInput.value.trim();
-        if (countryInput.value === "") {
+        const searchInput = countryInput.value.trim();
+        if (searchInput === "") { // Felmeddelande om man inte sökar på någonting
             console.log("Fyll i namn");
             searchError.innerHTML = "Vänligen, fyll i ett land";
             return;
-            /*alert("Vänligen, fyll i ett land")*/
         }
+        fetchCountry(searchInput, searchError);
+
     });
 });
 
-async function fetchCountry() {
-    const url = `https://restcountries.com/v3.1/name/${countryInput}`;
+/**
+ * Funktion för att hämta JSON-data via API beroende på vad användaren sökt för land
+ * @param {string} countryInput 
+ */
+async function fetchCountry(countryInput, searchError) {
+    searchError.innerHTML = ""; // Tar bort felmeddelande i DOM innan anropet sker
+
+    const url = `https://restcountries.com/v3.1/name/${countryInput}?fullText=false`;
     try {
         const response = await fetch(url);
         const data = await response.json();
+        // Generera felmeddelande om namnet inte hittas.
+        if (!response.ok) {
+            searchError.innerHTML = `Landet "${countryInput}" hittades inte, prova med namnet på Engelska.`
+        }
         console.log(data);
+        const currency = document.getElementById("currency-converter");
+        currency.innerHTML = "";
+        const currencyObject = data[0].currencies;
+        const currencyCode = Object.keys(currencyObject)[0];
+        currency.innerHTML = currencyCode;
+        console.log(currencyCode);
     } catch (error) {
         console.error("Felmeddelande: ", error);
     }
