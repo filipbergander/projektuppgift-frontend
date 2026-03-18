e7fabd8276135a32843c4c17b18a4fb243b95d46
Filip <filip.bergander1996@gmail.com>
2026-03-18 10:43:39 +0100

    Test för att se hur mycket font-inhämtning från Google fonts påverkar prestandan.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rmffaoa5h0s"
+    "revision": "0.29c7mmcd3v"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
