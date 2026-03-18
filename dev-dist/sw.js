23fe8effe9afd8ecb7ba610ee22f0c9f46ca3e50
Filip <filip.bergander1996@gmail.com>
2026-03-18 11:16:11 +0100

    Genererat JSDoc. Tagit tillbaka mina fonter som jag använde först.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0c0la3ip1s8"
+    "revision": "0.jq3bfr356fk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
